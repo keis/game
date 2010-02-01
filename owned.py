03ef8e8b80f6acb79a2e04ff6b6de84b4f0e6ab3
keis <keijser@gmail.com>
2010-02-01 23:05:21 +0100

    add hooks to Owned

--- owned.py
+++ owned.py
@@ -1,15 +1,15 @@
+from hook import Hookable
 
-class Owned(object):
+class Owned(Hookable):
 	def __init__(self, owner=None, **kwargs):
 		super(Owned, self).__init__(**kwargs)
 		self.owner = owner
 
 	def claim(self, owner):
-		if self.owner == None:
+		(owner,) = self.run_hook('pre-claim', owner)
+		if owner is not None:
 			self.owner = owner
-		else:
-			pass
-			# some fancy logic
+			self.run_hook('post-claim', owner)
 
 def friendly(a,b):
 	return getattr(a, 'owner', None) == getattr(b, 'owner', None)
