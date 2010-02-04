27232793bdfacf71b813e57c5495bdc2422ebab1
David Keijser <keijser@gmail.com>
2010-02-04 19:15:44 +0100

    untested idea of a effect

--- effects.py
+++ effects.py
@@ -10,3 +10,10 @@ def heal(source, building):
 	def _heal(b, power):
 		b.heal(source, power)
 	building.apply_aoe(_heal, lambda c,x: x * 0.8, 100)
+
+def charge(source, frm, to, scale=1.5):
+	power = {'value':0}
+	def _charge(b, p):
+		power['value'] += p
+	frm.apply_line(to, _charge, lambda c,x: x * scale, 100)
+	return power['value']
