3eace9f4dd600caf333d7cd013008083e413a7d5
David Keijser <keijser@gmail.com>
2010-04-16 16:26:17 +0200

    testing seed

--- seed.py
+++ seed.py
@@ -11,26 +11,34 @@ spells = (prepare_spell, focus_spell, repair, summon, construct, fire_storm)
 creatures = (LizardDemon, Skeleton, Summoner, ManaEater)
 buildings = (SmallManaRuby, MediumManaRuby, LargeManaRuby, Boulder)
 
+current_test = fire_storm
 starting_spells = (prepare_spell, prepare_spell, prepare_spell,
-	summon, summon, summon,
-	construct, construct,
-	focus_spell, focus_spell)
+	#summon, summon, summon,
+	#construct, construct,
+	focus_spell, focus_spell,
+	current_test, current_test, current_test,
+)
 
 hook_db = HookDB()
 
 def create_player():
 	m = Mage(hook_db = hook_db)
-	rubies = [MediumManaRuby(owner=m, hook_db=hook_db) for x in range(4)]
-	m.core = Core(owner=m, hook_db=hook_db)
-	spells = [x(owner=m, hook_db=hook_db) for x in starting_spells]
+	rubies = [MediumManaRuby(owner=m) for x in range(4)]
+	m.core = Core(owner=m)
+	spells = [x(owner=m) for x in starting_spells]
 	m.library.add(spells)
 
 	map(m.core.connect, rubies)
 	players.append(m)
 
+	test = Boulder(owner=m)
+	rubies[0].connect(test)
+	m.add_building(test)
+
 	l = ManaEater(owner=m, hook_db=hook_db, position=m.core)
 	m.add_creature(l)
 	m.core.add_creature(l)
+	map(m.add_building, [m.core] + rubies)
 
 	m.focus()
 
